9f161ec9f5cb0d11161f2a8ede2a7aefd4c47851
adenyuy <152186132+adenyuy@users.noreply.github.com>
2024-01-22 14:22:37 +0700

    Add files via upload

---
+++ proyek/main.js
@@ -0,0 +1,22 @@
+const videoC = document.querySelector(".populer .card .claude .link video");
+
+
+videoC.addEventListener("mouseenter", () => {
+  videoC.play();
+})
+
+videoC.addEventListener("mouseleave", () => {
+  videoC.pause();
+})
+
+const videoSLN = document.querySelector(".populer .card .selena .link video");
+
+
+video.addEventListener("mouseenter", () => {
+  video.play();
+})
+
+video.addEventListener("mouseleave", () => {
+  video.pause();
+})
+
