dadb6a330480465da4ac6085772e70d28e540d74
adenyuy <152186132+adenyuy@users.noreply.github.com>
2024-01-24 08:48:59 +0700

    revisi1

--- proyek/main.js
+++ proyek/main.js
@@ -12,11 +12,54 @@ videoC.addEventListener("mouseleave", () => {
 const videoSLN = document.querySelector(".populer .card .selena .link video");
 
 
-video.addEventListener("mouseenter", () => {
-  video.play();
+videoSLN.addEventListener("mouseenter", () => {
+  videoSLN.play();
 })
 
-video.addEventListener("mouseleave", () => {
-  video.pause();
+videoSLN.addEventListener("mouseleave", () => {
+  videoSLN.pause();
 })
 
+const videoGRG = document.querySelector(".populer .card .granger .link video");
+
+
+videoGRG.addEventListener("mouseenter", () => {
+  videoGRG.play();
+})
+
+videoGRG.addEventListener("mouseleave", () => {
+  videoGRG.pause();
+})
+
+const videoF = document.querySelector(".populer .card .floryn .link video");
+
+
+videoF.addEventListener("mouseenter", () => {
+  videoF.play();
+})
+
+videoF.addEventListener("mouseleave", () => {
+  videoF.pause();
+})
+
+const videoA = document.querySelector(".populer .card .angela .link video");
+
+
+videoA.addEventListener("mouseenter", () => {
+  videoA.play();
+})
+
+videoA.addEventListener("mouseleave", () => {
+  videoA.pause();
+})
+
+const videoCH = document.querySelector(".populer .card .change .link video");
+
+
+videoCH.addEventListener("mouseenter", () => {
+  videoCH.play();
+})
+
+videoCH.addEventListener("mouseleave", () => {
+  videoCH.pause();
+})
